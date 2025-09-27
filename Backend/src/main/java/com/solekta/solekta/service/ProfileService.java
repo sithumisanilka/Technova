15a9f20f49df8f44070081ad35deaeeb8b7a902d
IT24200368 <IT24200368@my.sliit.lk>
2025-09-27 11:57:48 +0530

    Add Profile service

--- Backend/src/main/java/com/solekta/solekta/service/ProfileService.java
+++ Backend/src/main/java/com/solekta/solekta/service/ProfileService.java
@@ -1,4 +1,20 @@
 package com.solekta.solekta.service;
 
+import com.solekta.solekta.model.Profile;
+import com.solekta.solekta.repository.ProfileRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public Profile createProfile(Profile profile) {
+        return profileRepository.save(profile);
+    }
 }
+
